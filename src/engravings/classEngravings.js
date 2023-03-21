94c5a5db3278c78ee68ab1f103ce318685221293
ja-me-ch <60724072+ja-me-ch@users.noreply.github.com>
2023-03-20 20:59:36 -0400

    added artist and related engravings

--- src/engravings/classEngravings.js
+++ src/engravings/classEngravings.js
@@ -1,67 +1,92 @@
 const classEngravings = [
     {
-        class: 'Berserker', engravings: ['Berserker\'s Technique', 'Mayhem']
+        class: "Berserker",
+        engravings: ["Berserker's Technique", "Mayhem"],
     },
     {
-        class: 'Paladin', engravings: ['Blessed Aura', 'Judgment']
+        class: "Paladin",
+        engravings: ["Blessed Aura", "Judgment"],
     },
     {
-        class: 'Gunlancer', engravings: ['Combat Readiness', 'Lone Knight']
+        class: "Gunlancer",
+        engravings: ["Combat Readiness", "Lone Knight"],
     },
     {
-        class: 'Destroyer', engravings: ['Gravity Training', 'Rage Hammer']
+        class: "Destroyer",
+        engravings: ["Gravity Training", "Rage Hammer"],
     },
     {
-        class: 'Striker', engravings: ['Deathblow', 'Esoteric Flurry']
+        class: "Striker",
+        engravings: ["Deathblow", "Esoteric Flurry"],
     },
     {
-        class: 'Wardancer', engravings: ['Esoteric Skill Enhancement', 'First Intention']
+        class: "Wardancer",
+        engravings: ["Esoteric Skill Enhancement", "First Intention"],
     },
     {
-        class: 'Scrapper', engravings: ['Shock Training', 'Ultimate Skill: Taijutsu']
+        class: "Scrapper",
+        engravings: ["Shock Training", "Ultimate Skill: Taijutsu"],
     },
     {
-        class: 'Soulfist', engravings: ['Energy Overflow', 'Robust Spirit']
+        class: "Soulfist",
+        engravings: ["Energy Overflow", "Robust Spirit"],
     },
     {
-        class: 'Glaivier', engravings: ['Control', 'Pinnacle']
+        class: "Glaivier",
+        engravings: ["Control", "Pinnacle"],
     },
     {
-        class: 'Gunslinger', engravings: ['Peacemaker', 'Time to Hunt']
+        class: "Gunslinger",
+        engravings: ["Peacemaker", "Time to Hunt"],
     },
     {
-        class: 'Artillerist', engravings: ['Barrage Enhancement', 'Firepower Enhancement']
+        class: "Artillerist",
+        engravings: ["Barrage Enhancement", "Firepower Enhancement"],
     },
     {
-        class: 'Deadeye', engravings: ['Enhanced Weapon', 'Pistoleer']
+        class: "Deadeye",
+        engravings: ["Enhanced Weapon", "Pistoleer"],
     },
     {
-        class: 'Sharpshooter', engravings: ['Death Strike', 'Loyal Companion']
+        class: "Sharpshooter",
+        engravings: ["Death Strike", "Loyal Companion"],
     },
     {
-        class: 'Machinist', engravings: ['Arthetinean Skill', 'Evolutionary Legacy']
+        class: "Machinist",
+        engravings: ["Arthetinean Skill", "Evolutionary Legacy"],
     },
     {
-        class: 'Bard', engravings: ['Desperate Salvation', 'True Courage']
+        class: "Bard",
+        engravings: ["Desperate Salvation", "True Courage"],
     },
     {
-        class: 'Sorceress', engravings: ['Igniter', 'Reflux']
+        class: "Sorceress",
+        engravings: ["Igniter", "Reflux"],
     },
     {
-        class: 'Arcanist', engravings: ['Empress\'s Grace', 'Order of the Emperor']
+        class: "Arcanist",
+        engravings: ["Empress's Grace", "Order of the Emperor"],
     },
     {
-        class: 'Summoner', engravings: ['Communication Overflow', 'Master Summoner']
+        class: "Summoner",
+        engravings: ["Communication Overflow", "Master Summoner"],
     },
     {
-        class: 'Shadowhunter', engravings: ['Demonic Impulse', 'Perfect Suppression']
+        class: "Shadowhunter",
+        engravings: ["Demonic Impulse", "Perfect Suppression"],
     },
     {
-        class: 'Deathblade', engravings: ['Surge', 'Remaining Energy']
+        class: "Deathblade",
+        engravings: ["Surge", "Remaining Energy"],
     },
     {
-        class: 'Reaper', engravings: ['Hunger', 'Lunar Voice']
-    }
-]
+        class: "Reaper",
+        engravings: ["Hunger", "Lunar Voice"],
+    },
+    {
+        class: "Artist",
+        engravings: ["Full Bloom", "Recurrence"],
+    },
+];
 
 export default classEngravings;
